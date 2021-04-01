ef07b8578ef3bcd9ce650f248b0a8c2ecc31b3c9
Fayal Ahmmed <faysalahmmed177@gmail.com>
2021-04-01 10:57:41 +0600

    create connection with mongodb

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const port = process.env.PORT || 4200
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7p4ls.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    console.log('connection eror', err);
+    const gadgetsCollection = client.db("gadgetsMart").collection("gadgets");
+    
+    console.log('database connected successfully');
+    
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
