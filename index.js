204db75390768493ff045063cae162ec623c658d
Fayal Ahmmed <faysalahmmed177@gmail.com>
2021-04-02 22:04:33 +0600

    project done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
 
 const port = process.env.PORT || 4200
 
@@ -24,9 +25,53 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
     console.log('connection eror', err);
     const gadgetsCollection = client.db("gadgetsMart").collection("gadgets");
-    
-    console.log('database connected successfully');
-    
+    const ordersCollection = client.db("gadgetsMart").collection("orders");
+
+    app.get('/products', (req, res) => {
+        gadgetsCollection.find()
+            .toArray((err, items) => {
+                console.log(items);
+                res.send(items)
+            })
+    })
+
+    app.get('/product/:id', (req, res) => {
+        console.log(req.params.id);
+        gadgetsCollection.find({ _id: ObjectId(req.params.id) })
+            .toArray((err, documents) => {
+                res.send(documents[0])
+            })
+    })
+
+   app.get('/orderedProduct', (req, res) =>{
+       ordersCollection.find({})
+       .toArray((err, documents) => {
+           console.log(documents);
+           res.send(documents);
+       })
+   })
+
+    app.post('/addProduct', (req, res) => {
+        const newProduct = req.body;
+        console.log('Adding new event', newProduct);
+        gadgetsCollection.insertOne(newProduct)
+            .then(result => {
+                console.log('Inserted count', result.insertedCount);
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+
+    app.post('/addOrder', (req, res) => {
+        const newOrder = req.body;
+        console.log('Adding new event', newOrder);
+        ordersCollection.insertOne(newOrder)
+            .then(result => {
+                console.log('Inserted count', result.insertedCount);
+                res.send(result.insertedCount > 0)
+            })
+    })
+
 });
 
 
